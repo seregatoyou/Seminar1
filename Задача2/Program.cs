d7e5078e66224b26179c37c8faea3f28d535219c
IT <spanyukhin@list.ru>
2023-10-12 16:32:19 +0300

    задачи 1-4, 1-2: только методы, 3-4 включены методы Main

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Внутри класса Answer напишите метод FindMax, 
+// который принимает на вход три числа и выдаёт максимальное из этих чисел.
+static void FindMax (int firstNumber, int secondNumber, int thirdNumber){
+        int result = firstNumber;
+        if (secondNumber>firstNumber){
+            result = secondNumber;
+        }
+        else if(thirdNumber>secondNumber){
+            result = thirdNumber;
+        }
+        else if(firstNumber>thirdNumber){
+            result = firstNumber;
+        }
+
+         System.Console.WriteLine($"{result}");
+    }
+FindMax(6, 6, 10);
---
+++ Задача4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Внутри класса Answer напишите метод PrintEvenNumbers, 
+// которая на вход принимает число (number), 
+// а на выходе выводит все чётные числа от 1 до number (включительно), 
+// разделеные знаком табуляции.
+using System;
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+for (int i = 2; i <= number; i+=2)
+{
+    System.Console.Write(i + "\t");
+}
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
